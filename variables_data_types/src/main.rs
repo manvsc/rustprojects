f7a3d68898d10bf851ea70879ea55756d2fecdd6
manvsc <79197645+manvsc@users.noreply.github.com>
2024-08-12 11:35:21 +0200

    added project

---
+++ variables_data_types/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6; // x needs to be mutable in order to change the value
+    println!("The value of x is: {x}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
+
+
+    // Shadowing:
+    
+}
